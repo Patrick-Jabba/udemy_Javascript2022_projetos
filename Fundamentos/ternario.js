d171505953c6ffc53d9e5f39cf1d2656346540ed
Patrick-Jabba <monteiromonterio@gmail.com>
2022-03-28 00:06:38 -0300

    estruturas de controle if, else, ternario

---
+++ Fundamentos/ternario.js
@@ -0,0 +1,5 @@
+const resultado = nota => nota >= 7 ? 'Aprovado' : 'Reprovado'
+//                       relacional    verdadeiro     falso
+console.log(resultado(7.1))
+console.log(resultado(6.7))
+//operador ternário é para retornar um dos dois valores (verdadeiro ou falso)
