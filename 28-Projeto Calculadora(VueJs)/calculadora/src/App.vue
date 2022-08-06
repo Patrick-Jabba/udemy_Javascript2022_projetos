f902a0aa593402c18f655d85bda00466ce5ad867
Patrick Monteiro Fischer <monteiromonterio@gmail.com>
2022-08-06 17:38:00 -0300

    Implementando a interface e captura de dados

---
+++ 28-Projeto Calculadora(VueJs)/calculadora/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="root">
+    <h1>Calculadora</h1>
+    <Calculator />
+  </div>
+</template>
+
+<script>
+import Calculator from "./main/Calculator.vue";
+export default {
+  components: { Calculator },
+};
+</script>
+
+<style>
+
+* {
+  font-family: sans-serif;
+}
+body {
+  margin: 0;
+}
+
+#root {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+
+  color: #fff;
+  background: linear-gradient(to right, rgb(83, 105, 118), rgb(41, 46, 73));
+}
+</style>
---
+++ 28-Projeto Calculadora(VueJs)/calculadora/src/main/Calculator.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="calculator">
+    <Display />
+    <Button label="AC" triple @onClick="clearMemory" />
+    <Button label="/" operation @onClick="setOperation" />
+    <Button label="7" @onClick="addDigit" />
+    <Button label="8" @onClick="addDigit" />
+    <Button label="9" @onClick="addDigit" />
+    <Button label="*" operation @onClick="setOperation" />
+    <Button label="4" @onClick="addDigit" />
+    <Button label="5" @onClick="addDigit" />
+    <Button label="6" @onClick="addDigit" />
+    <Button label="-" operation @onClick="setOperation" />
+    <Button label="1" @onClick="addDigit" />
+    <Button label="2" @onClick="addDigit" />
+    <Button label="3" @onClick="addDigit" />
+    <Button label="+" operation @onClick="setOperation" />
+    <Button label="0" double @onClick="addDigit" />
+    <Button label="." @onClick="addDigit" />
+    <Button label="=" operation @onClick="setOperation" />
+  </div>
+</template>
+<script>
+import Display from "../components/Display.vue";
+import Button from "../components/Button.vue";
+
+export default {
+  components: { Display, Button },
+  methods: {
+    clearMemory() {},
+    setOperation(operation) {},
+    addDigit(n) {},
+  },
+};
+</script>
+
+<style>
+.calculator {
+  height: 320px;
+  width: 235px;
+  border-radius: 5px;
+  overflow: hidden;
+
+  display: grid;
+  grid-template-columns: repeat(4, 25%);
+  grid-template-rows: 1fr 48px 48px 48px 48px 48px;
+}
+</style>
